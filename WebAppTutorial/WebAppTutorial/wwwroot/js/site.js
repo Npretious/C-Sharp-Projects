c8b6c93f623da69b2f2977082e4cb9d42038253c
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-07-23 19:11:18 -0600

    C# Assignments Push

---
+++ WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Functions to be called onmouseover and onmouseout to change the color of "Hello World!"
+function newColor() {
+    document.getElementById("Hello_World!").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("Hello_World!").style.color = "darkolivegreen";
+}
