18227bce2468ac68eb3eac16eb2ee4e869fe7cf7
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-07-29 14:16:35 -0600

    Boolean While Do Loop Assignment

---
+++ BooleanWhileDoAssignment/BooleanWhileDoAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BooleanWhileDoAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Below is a do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop");
+                count++;
+            }
+            while (count < 8); //loops until program reaches 8
+
+            //Below is a while loop
+            Console.WriteLine("Press enter to show while loop");
+            Console.ReadLine();
+
+            int counting = 0;
+            while (counting < 10) //loops until program counts reaches 10
+            {
+                Console.WriteLine("Ten at a time!");
+                counting++;
+            }
+        }
+    }
+}
