333fc7851cba29503a6669558f8c67e3b02486d4
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-11 13:24:06 -0600

    Simple Fantasy Game(Callings Methods Assignment) Push

---
+++ SimpleFantasyGame(Calling Methods Assignment)/CallingMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ouch! The Hell Warrior spikes you with a spell from the underworld!");
+            Console.WriteLine("Please enter your defense stat:");
+            int defense = Convert.ToInt32(Console.ReadLine());
+            int enemy_damage = MathMethods.Defense(defense);
+            Console.WriteLine("Dreadfully, you've taken " + enemy_damage + " damage points.");
+            Console.WriteLine("You instantly cast your prayer spells to heal.");
+            Console.WriteLine("Please enter your prayer stat:");
+            int prayer = Convert.ToInt32(Console.ReadLine());
+            int health = MathMethods.Prayer(prayer);
+            Console.WriteLine("You've been healed for " + health + " health points.");
+            Console.WriteLine("Now, plan your counter attack.");
+            Console.WriteLine("Please enter your attack stat:");
+            int strength = Convert.ToInt32(Console.ReadLine());
+            int damage = MathMethods.Attack(strength);
+            Console.WriteLine("You landed a significant shot and dealt " + damage + " damage points.");
+            Console.WriteLine("The Hell Warriors returns to the underworld after your fatal attack!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleFantasyGame(Calling Methods Assignment)/CallingMethods/StatMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class MathMethods
+    {
+        public static int Attack(int attack)
+        {
+            int weapon_pt = 10;
+            int enemy_def = 3;
+            int special_att = 2;
+            int crit_hit = 5;
+            int damage = (attack + (weapon_pt + special_att) * crit_hit - enemy_def);
+            return damage;
+        }
+        public static int Defense(int defense)
+        {
+            int enemy_att = 5;
+            int weapon_pt = 4;
+            int special_att = -3;
+            int crit_hit = 4;
+            int damage = (enemy_att + (weapon_pt + special_att) - defense) * crit_hit;
+            return damage;
+        }
+        public static int Prayer(int prayer)
+
+        {
+            int health = prayer + 5;
+            return health;
+        }
+    }
+}
