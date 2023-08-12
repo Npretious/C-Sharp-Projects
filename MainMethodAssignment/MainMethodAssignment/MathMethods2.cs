027da329786415037a4a72b2499d885e43e66167
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-12 13:32:15 -0600

    Main Method Assignment Push

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethods2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace MainMethodAssignment
+{
+        public class MathMethods2
+        {
+            //First method - takes an integer
+            public int MathOp(int number)
+            {
+                return number + 10;
+            }
+            //Second method - takes a string
+            public int MathOp(string number)
+            {
+                //Converts string to an integer to perform equation
+                int newnum = Convert.ToInt32(number);
+                return newnum % 5;
+            }
+            //Third method - takes a decimal
+            public int MathOp(decimal number)
+            {
+                //Converts decimal to an integer to perform equation
+                int newnum = Convert.ToInt32(number);
+                return newnum * 15;
+            }
+        }
+ 
+}
+
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the MathMethods2 Class
+            MathMethods2 math1 = new MathMethods2();
+            //Calls the first method with int parameter
+            Console.WriteLine(math1.MathOp(5));
+            //Calls the second method with string paramter
+            Console.WriteLine(math1.MathOp("5"));
+            //Calls the third method with decimal parameter
+            Console.WriteLine(math1.MathOp(5.1m));
+        }
+    }
+}
