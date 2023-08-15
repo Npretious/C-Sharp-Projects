524f9b26351a3fcf2f152432045c33f433bfdae7
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-14 21:48:16 -0600

    Method Class Assignment Submission Push

---
+++ MethodClassAssignmentSubmission/MethodClassAssignmentSubmission/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignmentSubmission
+{
+    class Class1
+    {
+        //created method to use two integers as parameters
+        public int MathOperation(int i, int j = 1)
+        {
+            int result = i * j;
+            return result;
+        }
+    }
+}
---
+++ MethodClassAssignmentSubmission/MethodClassAssignmentSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodClassAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //displays instructions for user
+            Console.WriteLine("Click enter to input two numbers for multiplication!");
+            Console.ReadLine();
+            //Instantiating the class
+            Class1 mathOp = new Class1();
+            //Asks user to enter first number
+            Console.WriteLine("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //Asks user to enter second number
+            Console.WriteLine("Enter another number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            //Calling on method class
+            int results = mathOp.MathOperation(num1, num2);
+            //displaying results on console
+            Console.WriteLine(num1 + " x " + num2 + " = " + results);
+        }
+    }
+}
