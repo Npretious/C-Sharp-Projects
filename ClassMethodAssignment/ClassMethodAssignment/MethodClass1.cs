042eae46cd489d8c94cb98a4ac8d78a042c819c4
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-15 18:29:26 -0600

    Class Method Assignment Push

---
+++ ClassMethodAssignment/ClassMethodAssignment/MethodClass1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodAssignment
+{
+    //Defines new class 
+    public class MethodClass1
+    {
+        //Defines a void method that takes two parameters x and y
+        public void Method(int x, int y)
+        {
+            //Displays values of x and y to console
+            Console.WriteLine("x: {0}, y: {1}", x, y);
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMethodAssignment
+{
+    //Defines new class
+    public class Program
+    {
+        //Defines main method
+        public static void Main()
+        {
+            //Instantating MethodClass1
+            MethodClass1 myObject = new MethodClass1();
+            //Calls on method on myObject instance, which passes both 1 and 2 as arguments
+            myObject.Method(1, 2);
+            //Calls on method again but specifies the arguments by name instead
+            myObject.Method(x: 3, y: 4);
+            //Prevents program from closing
+            Console.ReadLine();
+        }
+    }
+}
