16e1bd74bfdc1c3620e33268c600793577b4f43c
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-20 10:32:10 -0600

    Operators Assignment Submission Push

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    //Creates employee class
+    public class Employee
+    {
+        //Adds ids for employee class and first name and last name
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overloads the == operator to compare two employees by their id property
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+        //Overloads the != operator to compare two employees by their id property
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate two objects of Employee class and assigns values to properties
+            Employee employee1 = new Employee();
+            employee1.Id = 1;
+            employee1.FirstName = "Nick";
+            employee1.LastName = "Pretious";
+
+            Employee employee2 = new Employee();
+            employee2.Id = 2;
+            employee2.FirstName = "Carole";
+            employee2.LastName = "Sullivan";
+
+            //Compares the two employees using the overloaded == operator.
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("The two employee Id's are identical.");
+            }
+            else
+                Console.WriteLine("The two employee Id's are unique.");
+        }
+    }
+}
