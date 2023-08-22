34ea07e16f5c45a488f98bd507e722165924bf0a
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-21 18:48:23 -0600

    Struct Assignment Push

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    //Defines a struct called number
+    struct Number
+    {
+        //Adds property called Amount of data type decimal
+        public decimal Amount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates a object of data type Number
+            Number number = new Number();
+            //Assigns a mount to object's Amount property
+            number.Amount = 5.5m;
+            //Displays amount to console
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
