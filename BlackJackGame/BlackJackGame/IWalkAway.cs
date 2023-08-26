daf44dfa676df13cf6e7250d6231d864264692a6
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-08-26 13:56:12 -0600

    BlackJack Game Upload

---
+++ BlackJackGame/BlackJackGame/IWalkAway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame
+{
+    interface IWalkAway
+    {
+        public void WalkAway(Player player);
+    }
+}
---
+++ BlackJackGame/BlackJackGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Online Casino. What is your name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much will your bankroll be today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of BlackJack?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new BlackJackGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thanks for playing!");
+            }
+
+            Console.WriteLine("Feel free to look around casino. Bye for now!");
+            Console.Read();
+        }
+
+    }
+}
+
+
